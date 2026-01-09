4811aa986d99f8c90cf3247c2a8cc5c920a58bbe
Mukesh Kumar <mk3304413@gmail.com>
2026-01-10 00:34:09 +0530

    Initial frontend commit

---
+++ src/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="footer-fixed bg-success text-light text-center">
+      <p className="mb-1">© 2026 Todo App | MERN Stack Project</p>
+      <small>Developed by Mukesh Kumar</small>
+    </footer>
+  );
+}
